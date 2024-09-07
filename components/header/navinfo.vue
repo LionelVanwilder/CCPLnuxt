a35e6dafe050ead8ef89ca50dde5420d0c0547c4
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-09-07 20:17:07 +0200

    ok

---
+++ components/header/navinfo.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav><li><a>test nav</a></li></nav>
+</template>
+<script>
+ export default{
+        name: 'NavInfo'
+    }
+</script>
--- nuxt-ccpl/app.vue
+++ nuxt-ccpl/app.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Navinfo></Navinfo>
+    <h1>hello</h1>
   </div>
 </template>
+<script>
+
+import NavInfo from '../components/header/navinfo.vue'
+
+export default {
+ name: 'App',
+
+ components: {
+  NavInfo
+ },
+
+}
+</script>
