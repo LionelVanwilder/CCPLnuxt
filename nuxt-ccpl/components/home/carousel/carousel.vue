478047ae2c5ef774bf6c4770f41300279016be12
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-09-08 13:51:51 +0200

    Update carousel.vue

--- nuxt-ccpl/components/home/carousel/carousel.vue
+++ nuxt-ccpl/components/home/carousel/carousel.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="carousel-container"  
-                data-aos="fade-up"
-                data-aos-duration="1000"
-                >
-                <div class="align-title">
-                  <h2>{{ $t("carousel.title") }}</h2>
-                </div>
-                
+        data-aos="fade-up"
+        data-aos-duration="1000"
+    >
+      <div class="align-title">
+        <h2>{{ $t("carousel.title") }}</h2>
+      </div>
+      
+      <!-- Carousel Component -->
       <carousel 
+        ref="carousel"
         :items-to-show="1"
         :transition="500"
         :breakpoints="breakpoints"
-        >
+        v-if="pagesReady"
+      >
         <slide v-for="(item, index) in pages" :key="index">
           <a :href="'#slide-' + index" class="carousel__item" :class="'slide-' + index">
             <div class="container-news">
@@ -20,20 +23,15 @@
                 <div class="carousel__img-container">
                   <img :src="item.image">
                 </div>
-                
-                
               </router-link>
             </div>
           </a>
         </slide>
-        
   
         <template #addons>
           <navigation />
           <pagination />
-          
         </template>
-       
       </carousel>
       
     </div>
@@ -54,10 +52,27 @@
       Pagination,
       Navigation
     },
+    
+    data() {
+      return {
+        pagesReady: false,  // Indicateur de données prêtes
+        breakpoints: {
+          475: {
+            itemsToShow: 1.5,
+          },
+          515: {
+            itemsToShow: 2,
+          },
+          768: {
+            itemsToShow: 3,
+          },
+        }
+      };
+    },
   
     computed: {
-        pages(){
-          return[
+      pages() {
+        return [
           {
             title: this.$t('navbar.links.program'),
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
@@ -79,41 +94,28 @@
             color: '#29bf12',
             path: '/Collaborations/Partenaires'
           },
-  
           {
             title: this.$t('navbar.links.doc'),
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
             image: docImage,
             color: '#E71D36',
             path: '/SAFP/Surdite'
-          },
-  
-        
-          ]
-        }
+          }
+        ];
+      }
     },
   
-    data() {
-      return {
-      
-  
-        breakpoints: {
-          475: {
-            itemsToShow: 1.5,
-          },
-  
-          515: {
-            itemsToShow: 2,
-          },
-  
-          768: {
-            itemsToShow: 3,
-          },
-  
+    mounted() {
+      // Forcer la mise à jour du carousel après le montage du composant
+      this.pagesReady = true;
+      this.$nextTick(() => {
+        if (this.$refs.carousel) {
+          this.$refs.carousel.update();  // Force l'update du carousel
         }
-      };
+      });
     }
   };
   </script>
+  
   <style></style>
   
