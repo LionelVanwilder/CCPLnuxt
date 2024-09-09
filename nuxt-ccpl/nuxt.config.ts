a38f04f5471f0c8d82a720d8c5a63e998b3e9d30
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-09-09 09:50:20 +0200

    update speed

--- nuxt-ccpl/nuxt.config.ts
+++ nuxt-ccpl/nuxt.config.ts
@@ -30,7 +30,6 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
         { name: 'viewport', content: 'width=device-width,initial-scale=1' },
-        
         { hid: 'og:url', property: 'og:url', content: 'https://centrecomprendreetparler.vercel.app/' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:title', property: 'og:title', content: 'Accueil | CCP asbl' },
@@ -47,16 +46,17 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'preload', href: '/images/banniere-du-centre-comprendre-et-parler-mobile.webp', as: 'image' },
-        { rel: 'icon', href: '/img/logonew.2600a878.png' },
-        { rel: 'stylesheet', href: 'https://unpkg.com/aos@2.3.1/dist/aos.css' },
-        { rel: 'stylesheet', href: 'https://use.typekit.net/msf6xgq.css' },
+        { rel: 'preload', href: '/images/banniere-du-centre-comprendre-et-parler.webp', as: 'image' },
+        { rel: 'icon', href: '/images/icone.svg' },
+        { rel: 'preload', href: 'https://unpkg.com/aos@2.3.1/dist/aos.css', as: 'style', onload:"this.rel='stylesheet'" },
+        { rel: 'stylesheet', href: 'https://unpkg.com/aos@2.3.1/dist/aos.css', media: 'print', onload: "this.media='all'"},
+        { rel: 'preload', href: 'https://use.typekit.net/msf6xgq.css', as: 'style', onload: "this.rel='stylesheet'" },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
         { href: 'https://fonts.googleapis.com/css2?family=Nerko+One&family=Rubik:ital,wght@0,300..900;1,300..900&family=Varela+Round&display=swap', rel: 'stylesheet' }
       ],
       script: [
         { src: 'https://unpkg.com/aos@2.3.1/dist/aos.js', defer: true },
-        { src: 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', defer: true },
         { 
           innerHTML: `document.addEventListener('DOMContentLoaded', function () {
             AOS.init();
