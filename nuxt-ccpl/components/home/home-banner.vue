9578dc771f3fbd0c7d25a0be302b3b067a6d1a47
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-09-09 12:20:40 +0200

    ok

--- nuxt-ccpl/components/home/home-banner.vue
+++ nuxt-ccpl/components/home/home-banner.vue
@@ -18,7 +18,7 @@
 <style>
 .home-banner{
     background-image: url('../../public/images/banniere-du-centre-comprendre-et-parler.avif');
-    min-height: 75vh;
+    min-height: 65vh;
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
@@ -27,5 +27,30 @@
     justify-content: center;
     flex-direction: column;
     padding: 0 3rem;
+}
+
+@media all and (max-width: 769px){
+    .home-banner h1{
+        font-size: 2.5rem;
+    }
+    .home-banner{
+        min-height: 40vh;
+        background-image: url('../../public/images/banniere-du-centre-comprendre-et-parler-tablet.avif');
+    }
+}
+
+@media all and (max-width: 600px){
+    .home-banner h1{
+        font-size: 1.7rem;
+    }
+
+    
+}
+
+@media all and (max-width: 500px){
+    .home-banner{
+        
+        background-image: url('../../public/images/banniere-du-centre-comprendre-et-parler-mobile.avif');
+    }
 }
 </style>
