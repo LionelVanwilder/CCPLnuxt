444ce38c738338210fa4d4ba0e59b9138ee55873
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-09-09 21:55:55 +0200

    up

--- nuxt-ccpl/pages/Quisommesnous.vue
+++ nuxt-ccpl/pages/Quisommesnous.vue
@@ -2,6 +2,10 @@
     <div class="template-page-container">
         <BreadCrumbCom :breadcrumbs="breadcrumbs"></BreadCrumbCom>
     <div class="Pages-container-content">
+      <div class="skills-img-container">
+                <NuxtImg src="/images/logo-centre-comprendre-et-parler-couleur.webp" accesskey="" alt="logo du Centre Comprendre et Parler" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width="1300" height="350"/>
+            </div>
+            <h1>Le CCP asbl</h1>
         <h2>{{$t('AboutPage.title')}} "Comprendre et Parler"</h2>
         <p v-html="$t('AboutPage.content1')"></p>
         <p v-html="$t('AboutPage.content2')"></p>
@@ -60,6 +64,9 @@
     }
 </script>
 <style scoped>
+h1{
+  display: none;
+}
    h3{
     color: #222222;
    }
